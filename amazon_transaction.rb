dc6aa19fbdcc371b012a7b5cea8c89867278c893
katieroseschulz <katiekaterose13@gmail.com>
2018-07-30 15:14:57 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,8 @@
-# Code your cash register here!
+class Amazon # Code your cash register here!
+  def initialize(product, price, amazon_prime, print_receipt)
+    @product = product
+    @price = []
+    @amazon_prime = []
+    @print_receipt = []
+    
+    
